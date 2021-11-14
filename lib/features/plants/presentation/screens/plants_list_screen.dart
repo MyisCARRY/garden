73b14e2e7a817b11d5060afd66517d78bfb815b4
PiotrBialas <piotr.bialas@appvinio.com>
2021-11-14 12:30:11 +0100

    GAR-10: - change pagination widget

--- lib/features/plants/presentation/screens/plants_list_screen.dart
+++ lib/features/plants/presentation/screens/plants_list_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pagewise/flutter_pagewise.dart';
 import 'package:garden/core/helper/consts.dart';
 import 'package:garden/core/helper/extensions.dart';
 import 'package:garden/core/presentation/widgets/appbars/title_app_bar.dart';
 import 'package:garden/core/presentation/widgets/buttons/filled_button.dart';
 import 'package:garden/core/presentation/widgets/custom_empty_screen.dart';
 import 'package:garden/core/presentation/widgets/custom_error_widget.dart';
+import 'package:garden/core/presentation/widgets/custom_loading_widget.dart';
 import 'package:garden/core/presentation/widgets/notifications.dart';
 import 'package:garden/core/style/paddings.dart';
 import 'package:garden/features/plants/domain/entities/plant.dart';
@@ -13,8 +15,6 @@ import 'package:garden/features/plants/presentation/screens/plant_form_screen.da
 import 'package:garden/features/plants/presentation/widgets/plant_widget.dart';
 import 'package:garden/generated/l10n.dart';
 import 'package:garden/injection_container.dart';
-import 'package:pagination_view/bloc/pagination_bloc.dart';
-import 'package:pagination_view/pagination_view.dart';
 
 class PlantsListScreen extends StatefulWidget {
   const PlantsListScreen({Key? key}) : super(key: key);
@@ -24,18 +24,21 @@ class PlantsListScreen extends StatefulWidget {
 }
 
 class _PlantsListScreenState extends State<PlantsListScreen> {
-  late final PaginationCubit _paginationCubit;
+  late final PagewiseLoadController<Plant> _pagewiseController;
 
   @override
   void initState() {
-    _paginationCubit = PaginationCubit([], _fetch);
+    _pagewiseController = PagewiseLoadController<Plant>(
+      pageFuture: _fetch,
+      pageSize: Consts.plantListPageSize,
+    );
 
     super.initState();
   }
 
   @override
   void dispose() {
-    _paginationCubit.close();
+    _pagewiseController.dispose();
 
     super.dispose();
   }
@@ -52,7 +55,7 @@ class _PlantsListScreenState extends State<PlantsListScreen> {
         child: Column(
           children: [
             Expanded(
-              child: PaginationView<Plant>(
+              child: PagewiseListView<Plant>(
                 itemBuilder: (BuildContext context, Plant plant, int i) => Padding(
                   padding: Paddings.horizontal24.overrideZeros(Paddings.vertical8),
                   child: PlantWidget(
@@ -63,16 +66,18 @@ class _PlantsListScreenState extends State<PlantsListScreen> {
                     ).addScreen(context),
                   ),
                 ),
-                pageFetch: _fetch,
-                onEmpty: const CustomEmptyWidget(),
-                onError: (error) => CustomErrorWidget(onRefresh: _paginationCubit.refreshPaginatedList),
+                showRetry: false,
+                errorBuilder: (_, __) => CustomErrorWidget(onRefresh: _pagewiseController.retry),
+                noItemsFoundBuilder: (_) => const CustomEmptyWidget(),
+                loadingBuilder: (_) => const CustomLoadingWidget(),
+                pageLoadController: _pagewiseController,
               ),
             ),
             const SizedBox(height: 12.0),
             Padding(
               padding: Paddings.horizontal24,
               child: FilledButton(
-                text: 'Add plant to garden',
+                text: S.current.addPlantToGarden.capitalize,
                 onTap: () => PlantFormScreen(
                   onSave: () => onSuccessSave(S.current.successPlantAdd),
                 ).addScreen(context),
@@ -85,10 +90,8 @@ class _PlantsListScreenState extends State<PlantsListScreen> {
     );
   }
 
-  Future<List<Plant>> Function(int) get _fetch => (int currentListSize) async {
-        final int page = (currentListSize / Consts.plantListPageSize).ceil();
-
-        final result = await sl<GetPlantsPageUsecase>()(page);
+  Future<List<Plant>> Function(int?) get _fetch => (int? page) async {
+        final result = await sl<GetPlantsPageUsecase>()(page ?? 10000);
 
         // throwing exceptions triggers onError in PaginationView
         return result.fold(
@@ -100,6 +103,6 @@ class _PlantsListScreenState extends State<PlantsListScreen> {
   void onSuccessSave(String text) {
     Navigator.of(context).maybePop();
     Notifications.neutral(description: text);
-    _paginationCubit.refreshPaginatedList();
+    _pagewiseController.reset();
   }
 }
